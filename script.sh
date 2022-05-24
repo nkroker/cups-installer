68971d8839896bea99a157515efef6469f8834cc
Nikhil Kumar Tyagi <nikhilq.75@gmail.com>
2022-05-24 12:37:59 +0530

    Added script

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo dpkg --add-architecture i386
+wget http://security.ubuntu.com/ubuntu/pool/main/c/cups/libcups2_2.1.3-4ubuntu0.11_amd64.deb
+sudo dpkg -i ./libcups2_2.1.3-4ubuntu0.11_amd64.deb
+sudo apt-get -f install
