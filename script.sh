1676a2f80053c5be213c477859e2218b206e8273
Nikhil Kumar Tyagi <nikhilq.75@gmail.com>
2022-05-24 12:41:34 +0530

    Updated README

--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@
 sudo dpkg --add-architecture i386
 wget http://security.ubuntu.com/ubuntu/pool/main/c/cups/libcups2_2.1.3-4ubuntu0.11_amd64.deb
 sudo dpkg -i ./libcups2_2.1.3-4ubuntu0.11_amd64.deb
-sudo apt-get -f install
+sudo apt-get -y -f install
